792fc194c4d973ec91d4d11c8b72a4024650b73e
novagc <novagc@yandex.ru>
2020-08-06 22:26:17 +0900

    fix bot

--- TelegramPostingBot/Bot/BasicBot.cs
+++ TelegramPostingBot/Bot/BasicBot.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
 using System.Text;
 
 using Telegram.Bot;
@@ -16,8 +17,8 @@ namespace TelegramPostingBot.Bot
 {
     class BasicBot :IBot
     {
-        public string BotToken { get; private set; }
-        public string ChannelID { get; private set; }
+        public string BotToken { get; }
+        public string ChannelID { get; }
 
         private ITelegramBotClient bot;
 
@@ -26,7 +27,7 @@ namespace TelegramPostingBot.Bot
             BotToken = token;
             ChannelID = channelID;
 
-            bot = new TelegramBotClient(BotToken);
+            bot = new TelegramBotClient(BotToken, new HttpClient());
         }
 
         public void SendPost(IEnumerable<IPostModel> posts)
@@ -42,39 +43,43 @@ namespace TelegramPostingBot.Bot
             if (post == null)
                 throw new ArgumentNullException("post");
 
-            switch (post.Type)
+            try
             {
-                case PostType.Text:
-                    SendTextPost(post);
-                    break;
+                switch (post.Type)
+                {
+                    case PostType.Text:
+                        SendTextPost(post);
+                        break;
 
-                case PostType.Image:
-                    SendImagePost(post);
-                    break;
+                    case PostType.Image:
+                        SendImagePost(post);
+                        break;
 
-                case PostType.Video:
-                    SendVideoPost(post);
-                    break;
+                    case PostType.Video:
+                        SendVideoPost(post);
+                        break;
 
-                case PostType.Images:
-                    SendImagesPost(post);
-                    break;
+                    case PostType.Images:
+                        SendImagesPost(post);
+                        break;
 
-                case PostType.Videos:
-                    SendVideosPost(post);
-                    break;
+                    case PostType.Videos:
+                        SendVideosPost(post);
+                        break;
 
-                case PostType.Audio:
-                    SendAudioPost(post);
-                    break;
+                    case PostType.Audio:
+                        SendAudioPost(post);
+                        break;
 
-                case PostType.Contact:
-                    SendContactPost(post);
-                    break;
+                    case PostType.Contact:
+                        SendContactPost(post);
+                        break;
 
-                case PostType.Sticker:
-                    SendStickerPost(post);
-                    break;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
             }
         }
 
@@ -83,7 +88,13 @@ namespace TelegramPostingBot.Bot
             if (post.Text == null)
                 throw new ArgumentException("Text Post: post.text = null");
 
-            bot.SendTextMessageAsync(ChannelID, post.Text, ParseMode.MarkdownV2);
+            var temp = bot.SendTextMessageAsync(
+                ChannelID, 
+                post.Text, 
+                ParseMode.Markdown);
+
+            temp.Wait();
+            Console.WriteLine(temp.Result);
         }
 
         private void SendImagePost(IPostModel post)
@@ -97,12 +108,14 @@ namespace TelegramPostingBot.Bot
             if (post.Attachments[0] == null)
                 throw new ArgumentException("Images Post: post.attachments[0] = null");
 
-            bot.SendPhotoAsync(
+            var temp = bot.SendPhotoAsync(
                 ChannelID, 
                 new InputMedia(post.Attachments[0]), 
                 post.Text, 
-                ParseMode.MarkdownV2
-                );
+                ParseMode.Markdown);
+
+            temp.Wait();
+            Console.WriteLine(temp.Result);
         }
 
         private void SendVideoPost(IPostModel post)
@@ -116,12 +129,14 @@ namespace TelegramPostingBot.Bot
             if (post.Attachments[0] == null)
                 throw new ArgumentException("Images Post: post.attachments[0] = null");
 
-            bot.SendVideoAsync(
+            var temp = bot.SendVideoAsync(
                 ChannelID, 
                 new InputMedia(post.Attachments[0]), 
                 caption: post.Text, 
-                parseMode: ParseMode.MarkdownV2
-                );
+                parseMode: ParseMode.Markdown);
+
+            temp.Wait();
+            Console.WriteLine(temp.Result.Text);
         }
 
         private void SendImagesPost(IPostModel post)
@@ -132,11 +147,12 @@ namespace TelegramPostingBot.Bot
             if (post.Attachments.Length < 2)
                 throw new ArgumentException("Images Post: post.attachments.length < 2");
 
-            bot.SendMediaGroupAsync(
-                post.Attachments
-                    .Select(x => new InputMediaPhoto(new InputMedia(x))),
-                ChannelID
-            );
+            var temp = bot.SendMediaGroupAsync(
+                post.Attachments.Select(x => new InputMediaPhoto(new InputMedia(x))),
+                ChannelID);
+
+            temp.Wait();
+            Console.WriteLine(temp.Result);
         }
 
         private void SendVideosPost(IPostModel post)
@@ -147,11 +163,12 @@ namespace TelegramPostingBot.Bot
             if (post.Attachments.Length < 2)
                 throw new ArgumentException("Images Post: post.attachments.length < 2");
 
-            bot.SendMediaGroupAsync(
-                post.Attachments
-                    .Select(x => new InputMediaVideo(new InputMedia(x))),
-                ChannelID
-            );
+            var temp = bot.SendMediaGroupAsync(
+                post.Attachments.Select(x => new InputMediaVideo(new InputMedia(x))),
+                ChannelID);
+
+            temp.Wait();
+            Console.WriteLine(temp.Result);
         }
 
         private void SendAudioPost(IPostModel post)
@@ -162,7 +179,13 @@ namespace TelegramPostingBot.Bot
             if (post.Attachments.Length == 0)
                 throw new ArgumentException("Images Post: post.attachments.length = 0");
 
-            bot.SendAudioAsync(ChannelID, new InputMedia(post.Attachments[0]), post.Text);
+            var temp = bot.SendAudioAsync(
+                ChannelID, 
+                new InputMedia(post.Attachments[0]),
+                post.Text);
+
+            temp.Wait();
+            Console.WriteLine(temp.Result.Text);
         }
 
         private void SendContactPost(IPostModel post)
@@ -176,10 +199,13 @@ namespace TelegramPostingBot.Bot
             if (post.Attachments.Length == 0)
                 throw new ArgumentException("Images Post: post.attachments.length = 0");
 
-            bot.SendContactAsync(
+            var temp = bot.SendContactAsync(
                 ChannelID, 
                 post.Attachments[0], 
                 post.Text);
+
+            temp.Wait();
+            Console.WriteLine(temp.Result);
         }
 
         private void SendStickerPost(IPostModel post)
@@ -193,10 +219,12 @@ namespace TelegramPostingBot.Bot
             if (post.Attachments[0] == null)
                 throw new ArgumentException("Images Post: post.attachments[0] = null");
 
-            bot.SendStickerAsync(
+            var temp = bot.SendStickerAsync(
                 ChannelID,
-                new InputMedia(post.Attachments[0])
-            );
+                new InputMedia(post.Attachments[0]));
+
+            temp.Wait();
+            Console.WriteLine(temp.Result.Text);
         }
     }
 }
